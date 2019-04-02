94e5985e1e6b7f9ad5f8a02ccfdb78db4f71dba4
Jimmy Amash <jimmy@hellojumper.com>
2019-04-01 23:23:25 -0700

    Redesign

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
+    if ($("#mainNav").offset().top > 20) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
